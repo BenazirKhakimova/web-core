2f79c37ba956fad82447eed9a3a0617e07e7c4bc
benazir <benazirkhakimova91@gmail.com>
2022-11-18 21:23:54 +0600

    have finished

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -1,8 +1,4 @@
 import './swiper'
-<<<<<<< HEAD
-
-=======
->>>>>>> b757b951dbc1629c0dfa24a7f9ad0fdd7bf046c9
 import '../scss/header.scss'
 import '../scss/button.scss'
 import '../scss/btn-slider.scss'
@@ -10,10 +6,6 @@ import '../scss/description.scss'
 import '../scss/show-more.scss'
 import '../scss/slide.scss'
 import '../scss/menu.scss'
-<<<<<<< HEAD
-=======
-import '../scss/tech.scss'
->>>>>>> b757b951dbc1629c0dfa24a7f9ad0fdd7bf046c9
 import '../scss/prices.scss'
 import '../scss/footer.scss'
 import '../scss/feedback.scss'
@@ -26,13 +18,8 @@ let btnCall = document.getElementsByClassName('btn__call')
 let btnMassage = document.getElementsByClassName('btn__massage')
 let massageModal = document.querySelector('.massage-modal')
 let callModal = document.querySelector('.call-modal')
-<<<<<<< HEAD
 let massageCloseBtn = document.querySelector('.massage_close')
 let callCloseBtn = document.querySelector('.call_close')
-=======
-let feedbackCloseBtn = document.querySelector('.feedback_close')
-let desktopCloseBtn = document.querySelector('.desktop__btn')
->>>>>>> b757b951dbc1629c0dfa24a7f9ad0fdd7bf046c9
 
 const handleMenu = () => {
   openMenu.addEventListener('click', function () {
@@ -47,22 +34,6 @@ const handleMenu = () => {
 }
 handleMenu()
 
-<<<<<<< HEAD
-=======
-const showDeskCloseButton = function () {
-  if (window.innerWidth >= 768) {
-    desktopCloseBtn.style.transform = 'translateX(0)'
-    desktopCloseBtn.style.transition = 'transform 0.2s linear'
-  }
-}
-const hideDeskCloseButton = function () {
-  if (window.innerWidth >= 768) {
-    desktopCloseBtn.style.transform = 'translateX(1000px)'
-    desktopCloseBtn.style.transition = 'transform 0.2s linear'
-  }
-}
-
->>>>>>> b757b951dbc1629c0dfa24a7f9ad0fdd7bf046c9
 const handleFeedbackMenu = () => {
   for (let i = 0; i < btnMassage.length; i++) {
     btnMassage[i].addEventListener('click', function () {
@@ -84,7 +55,6 @@ const handleFeedbackMenu = () => {
     })
   }
 
-<<<<<<< HEAD
   massageCloseBtn.addEventListener('click', function () {
     massageModal.classList.remove('feedback_open')
     menuBg.style.display = 'none'
@@ -92,10 +62,6 @@ const handleFeedbackMenu = () => {
     hideDeskCloseButton()
   })
   callCloseBtn.addEventListener('click', function () {
-=======
-  feedbackCloseBtn.addEventListener('click', function () {
-    massageModal.classList.remove('feedback_open')
->>>>>>> b757b951dbc1629c0dfa24a7f9ad0fdd7bf046c9
     callModal.classList.remove('feedback_open')
     menuBg.style.display = 'none'
 
